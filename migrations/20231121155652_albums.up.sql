0aae9d52951bf048779f125e8bd8215dad772cca
Luis Dourado <lfaddourado@gmail.com>
2023-11-21 17:08:51 -0300

    fix: add not null to description

--- migrations/20231121155652_albums.up.sql
+++ migrations/20231121155652_albums.up.sql
@@ -3,7 +3,7 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS albums (
   "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   "title" VARCHAR(255) NOT NULL,
-  "description" TEXT,
+  "description" TEXT NOT NULL,
   "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
