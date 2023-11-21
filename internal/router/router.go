5ae9ed9c82a1694c6c2ec89dc681530c263521a9
Luis Dourado <lfaddourado@gmail.com>
2023-11-21 17:09:37 -0300

    fix: change route structure to comport dynamic routes

--- internal/router/router.go
+++ internal/router/router.go
@@ -23,21 +23,37 @@ func Routes() http.Handler{
         MaxAge: 300,
     }))
     router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte("Api Desafio do Candidato Sensedia"))
+        w.Write([]byte("API Root"))
     })
-    // Users routes
-    router.Get("/api/v1/users", controllers.GetAllUsers)
-    router.Get("/api/v1/users/{id}", controllers.GetUserByID)
-    router.Put("/api/v1/users/{id}", controllers.UpdateUser)
-    router.Put("/api/v1/users/{id}", controllers.DeleteUser)
-    router.Post("/api/v1/users/create", controllers.CreateUser)
 
-    // Albums routes
-    router.Get("/api/v1/albums", controllers.GetAllAlbums)
+    // User routes
+    router.Route("/api/v1/users", func(r chi.Router) {
+        r.Get("/", controllers.GetAllUsers)                   // GET /api/v1/users
+        r.Post("/create", controllers.CreateUser) 
+        r.Route("/{id}", func(r chi.Router) {
+            r.Get("/", controllers.GetUserByID)  
+            r.Put("/", controllers.UpdateUser)          
+            r.Delete("/", controllers.DeleteUser)     
+        })
+    })
 
+    // Album routes
+    router.Route("/api/v1/albums", func(r chi.Router) {
+        r.Get("/", controllers.GetAllAlbums)                 
+        r.Post("/create", controllers.CreateAlbum)            
+        r.Route("/{id}", func(r chi.Router) {
+            r.Get("/", controllers.GetAlbumByID)             
+            r.Put("/", controllers.UpdateAlbum)              
+            r.Delete("/", controllers.DeleteAlbum)        
+        
+        })
+    })
 
-  
-
+    // User Albums routes
+    router.Route("/api/v1/{user_id}/albums", func(r chi.Router) {
+        r.Get("/", controllers.GetAlbumsByUserID)  
+        r.Delete("/{album_id}", controllers.RemoveAlbumFromUser) 
+    })
     // Add the route that serves the Swagger UI
 
     return router
